52785953ace35fd4a1f5c51832db5b782dea5a09
Adithyavsabu <adithyav@blueehr.com>
2024-07-22 15:34:21 +0530

    first commit

---
+++ image gallery/js/script.js
@@ -0,0 +1,33 @@
+const textArray = [
+  "https://assets.codepen.io/1506195/unsplash-music-0.avif",
+  "https://assets.codepen.io/1506195/unsplash-music-1.avif",
+  "https://assets.codepen.io/1506195/unsplash-music-2.avif",
+  "https://assets.codepen.io/1506195/unsplash-music-3.avif",
+  "https://assets.codepen.io/1506195/unsplash-music-4.avif",
+  "https://assets.codepen.io/1506195/unsplash-music-5.avif",
+  "https://assets.codepen.io/1506195/unsplash-music-6.avif",
+  "https://assets.codepen.io/1506195/unsplash-music-7.avif",
+];
+
+// Create a container and apply styles via a CSS class
+const container = document.createElement("div");
+
+const div = document.createElement("div");
+div.className = "gallery";
+
+// Create an array to hold the elements
+const elements = textArray.map((text) => {
+  const img = document.createElement("img");
+  img.src = text;
+  div.appendChild(img);
+
+  return div;
+});
+
+// Create a DocumentFragment to batch DOM updates
+const fragment = document.createDocumentFragment();
+fragment.append(...elements); // Append all elements at once
+
+// Append the fragment to the container and the container to the body
+container.appendChild(fragment);
+document.body.appendChild(container);
